5122ceb47cb991b9deefc60e8f629feb15412758
r-muthu <muthuramaswamy2003@gmail.com>
2026-07-27 13:25:48 +0800

    add frontend link to cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://studypilot-ai-pink.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
